25104398a6c48d8b15e12a66f11c8161bf6da4ff
Zhuguanyu Wu <goatwu0415@gmail.com>
2025-05-08 16:44:20 +0800

    bug fixed for causvid script (#31)

--- scripts/run_wan_t2v_causvid.sh
+++ scripts/run_wan_t2v_causvid.sh
@@ -32,7 +32,7 @@ python -m lightx2v.infer \
 --model_cls wan2.1_causvid \
 --task t2v \
 --model_path $model_path \
---config_json ${lightx2v_path}/configs/wan_t2v_causal.json \
+--config_json ${lightx2v_path}/configs/wan_t2v_causvid.json \
 --prompt "Two anthropomorphic cats fight intensely on a spotlighted stage; the left cat wearing blue boxing gear with matching gloves, the right cat in bright red boxing attire and gloves." \
 --negative_prompt 色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走 \
 --save_video_path ${lightx2v_path}/save_results/output_lightx2v_wan_t2v_causal.mp4
