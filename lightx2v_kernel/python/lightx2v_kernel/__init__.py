629d21a1b0d012f98cbf40a20516bbb8fcc0d96e
helloyongyang <yongyang1030@163.com>
2025-06-23 20:09:01 +0800

    fix ci

--- lightx2v_kernel/python/lightx2v_kernel/__init__.py
+++ lightx2v_kernel/python/lightx2v_kernel/__init__.py
@@ -1,8 +1,11 @@
 import ctypes
 import os
 import platform
-
 import torch
+from lightx2v_kernel import common_ops
+from lightx2v_kernel.gemm import cutlass_scaled_fp4_mm, scaled_fp4_quant
+from lightx2v_kernel.version import __version__
+
 
 SYSTEM_ARCH = platform.machine()
 
@@ -10,8 +13,5 @@ cuda_path = f"/usr/local/cuda/targets/{SYSTEM_ARCH}-linux/lib/libcudart.so.12"
 if os.path.exists(cuda_path):
     ctypes.CDLL(cuda_path, mode=ctypes.RTLD_GLOBAL)
 
-from lightx2v_kernel import common_ops
-from lightx2v_kernel.gemm import cutlass_scaled_fp4_mm, scaled_fp4_quant
-from lightx2v_kernel.version import __version__
 
 build_tree_kernel = None
