4f5edb8191f89180adf37699c4fb53cac6e327ef
helloyongyang <yongyang1030@163.com>
2025-07-16 19:49:14 +0800

    update scripts

--- scripts/dist_infer/run_wan_t2v_dist_ulysses.sh
+++ scripts/dist_infer/run_wan_t2v_dist_ulysses.sh
@@ -6,7 +6,7 @@ model_path=
 
 # check section
 if [ -z "${CUDA_VISIBLE_DEVICES}" ]; then
-    cuda_devices=1,2,3,4
+    cuda_devices=0,1,2,3
     echo "Warn: CUDA_VISIBLE_DEVICES is not set, using default value: ${cuda_devices}, change at shell script or set env variable."
     export CUDA_VISIBLE_DEVICES=${cuda_devices}
 fi
