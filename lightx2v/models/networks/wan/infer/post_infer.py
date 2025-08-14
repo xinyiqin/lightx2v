95b58beb73c77c989864d717134c2198205b1162
helloyongyang <yongyang1030@163.com>
2025-08-14 14:18:04 +0800

    update parallel

--- lightx2v/models/networks/wan/infer/post_infer.py
+++ lightx2v/models/networks/wan/infer/post_infer.py
@@ -10,35 +10,15 @@ class WanPostInfer:
         self.out_dim = config["out_dim"]
         self.patch_size = (1, 2, 2)
         self.clean_cuda_cache = config.get("clean_cuda_cache", False)
-        self.infer_dtype = GET_DTYPE()
-        self.sensitive_layer_dtype = GET_SENSITIVE_DTYPE()
 
     def set_scheduler(self, scheduler):
         self.scheduler = scheduler
 
     @torch.compile(disable=not CHECK_ENABLE_GRAPH_MODE())
-    def infer(self, weights, x, e, grid_sizes):
-        if e.dim() == 2:
-            modulation = weights.head_modulation.tensor  # 1, 2, dim
-            e = (modulation + e.unsqueeze(1)).chunk(2, dim=1)
-        elif e.dim() == 3:  # For Diffustion forcing
-            modulation = weights.head_modulation.tensor.unsqueeze(2)  # 1, 2, seq, dim
-            e = (modulation + e.unsqueeze(1)).chunk(2, dim=1)
-            e = [ei.squeeze(1) for ei in e]
-
-        x = weights.norm.apply(x)
-
-        if self.sensitive_layer_dtype != self.infer_dtype:
-            x = x.to(self.sensitive_layer_dtype)
-        x.mul_(1 + e[1].squeeze()).add_(e[0].squeeze())
-        if self.sensitive_layer_dtype != self.infer_dtype:
-            x = x.to(self.infer_dtype)
-
-        x = weights.head.apply(x)
-        x = self.unpatchify(x, grid_sizes)
+    def infer(self, weights, x, pre_infer_out):
+        x = self.unpatchify(x, pre_infer_out.grid_sizes)
 
         if self.clean_cuda_cache:
-            del e, grid_sizes
             torch.cuda.empty_cache()
 
         return [u.float() for u in x]
