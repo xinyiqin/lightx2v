79c3caa26ae753461cb0ebe00bd235b6b5a75444
Watebear <wushuo@bupt.cn>
2025-08-20 21:36:38 +0800

    feature: support qwen-image-edit(i2i) (#234)
    * feature: support qwen-image-edit(i2i)
    
    * bugfix

--- lightx2v/models/input_encoders/hf/qwen25/qwen25_vlforconditionalgeneration.py
+++ lightx2v/models/input_encoders/hf/qwen25/qwen25_vlforconditionalgeneration.py
@@ -1,7 +1,45 @@
+import math
 import os
 
 import torch
 from transformers import Qwen2Tokenizer, Qwen2_5_VLForConditionalGeneration
+
+try:
+    from diffusers.image_processor import VaeImageProcessor
+    from transformers import Qwen2VLProcessor
+except ImportError:
+    VaeImageProcessor = None
+    Qwen2VLProcessor = None
+
+PREFERRED_QWENIMAGE_RESOLUTIONS = [
+    (672, 1568),
+    (688, 1504),
+    (720, 1456),
+    (752, 1392),
+    (800, 1328),
+    (832, 1248),
+    (880, 1184),
+    (944, 1104),
+    (1024, 1024),
+    (1104, 944),
+    (1184, 880),
+    (1248, 832),
+    (1328, 800),
+    (1392, 752),
+    (1456, 720),
+    (1504, 688),
+    (1568, 672),
+]
+
+
+def calculate_dimensions(target_area, ratio):
+    width = math.sqrt(target_area * ratio)
+    height = width / ratio
+
+    width = round(width / 32) * 32
+    height = round(height / 32) * 32
+
+    return width, height, None
 
 
 class Qwen25_VLForConditionalGeneration_TextEncoder:
@@ -11,8 +49,12 @@ class Qwen25_VLForConditionalGeneration_TextEncoder:
         self.tokenizer = Qwen2Tokenizer.from_pretrained(os.path.join(config.model_path, "tokenizer"))
 
         self.tokenizer_max_length = 1024
-        self.prompt_template_encode = "<|im_start|>system\nDescribe the image by detailing the color, shape, size, texture, quantity, text, spatial relationships of the objects and background:<|im_end|>\n<|im_start|>user\n{}<|im_end|>\n<|im_start|>assistant\n"
-        self.prompt_template_encode_start_idx = 34
+        self.prompt_template_encode = config.prompt_template_encode
+        self.prompt_template_encode_start_idx = config.prompt_template_encode_start_idx
+
+        if config.task == "i2i":
+            self.image_processor = VaeImageProcessor(vae_scale_factor=self.config["vae_scale_factor"] * 2)
+            self.processor = Qwen2VLProcessor.from_pretrained(os.path.join(config.model_path, "processor"))
 
         self.device = torch.device("cuda")
         self.dtype = torch.bfloat16
@@ -24,19 +66,63 @@ class Qwen25_VLForConditionalGeneration_TextEncoder:
         split_result = torch.split(selected, valid_lengths.tolist(), dim=0)
         return split_result
 
-    def infer(self, text):
+    def preprocess_image(self, image):
+        image_size = image.size
+        width, height = image_size
+        calculated_width, calculated_height, _ = calculate_dimensions(1024 * 1024, width / height)
+
+        height = height or calculated_height
+        width = width or calculated_width
+
+        multiple_of = self.config.vae_scale_factor * 2
+        width = width // multiple_of * multiple_of
+        height = height // multiple_of * multiple_of
+
+        if image is not None and not (isinstance(image, torch.Tensor) and image.size(1) == self.latent_channels):
+            image_height, image_width = self.image_processor.get_default_height_width(image)
+            aspect_ratio = image_width / image_height
+
+            if self.config._auto_resize:
+                _, image_width, image_height = min((abs(aspect_ratio - w / h), w, h) for w, h in PREFERRED_QWENIMAGE_RESOLUTIONS)
+            image_width = image_width // multiple_of * multiple_of
+            image_height = image_height // multiple_of * multiple_of
+            image = self.image_processor.resize(image, image_height, image_width)
+            prompt_image = image
+            image = self.image_processor.preprocess(image, image_height, image_width)
+            image = image.unsqueeze(2)
+        return prompt_image, image, (image_height, image_width)
+
+    def infer(self, text, image=None):
         template = self.prompt_template_encode
         drop_idx = self.prompt_template_encode_start_idx
         txt = [template.format(e) for e in text]
-        txt_tokens = self.tokenizer(txt, max_length=self.tokenizer_max_length + drop_idx, padding=True, truncation=True, return_tensors="pt").to(self.device)
 
-        encoder_hidden_states = self.text_encoder(
-            input_ids=txt_tokens.input_ids,
-            attention_mask=txt_tokens.attention_mask,
-            output_hidden_states=True,
-        )
+        if image is not None:
+            prompt_image, image, image_info = self.preprocess_image(image)
+            model_inputs = self.processor(
+                text=txt,
+                images=prompt_image,
+                padding=True,
+                return_tensors="pt",
+            ).to(self.device)
+            encoder_hidden_states = self.text_encoder(
+                input_ids=model_inputs.input_ids,
+                attention_mask=model_inputs.attention_mask,
+                pixel_values=model_inputs.pixel_values,
+                image_grid_thw=model_inputs.image_grid_thw,
+                output_hidden_states=True,
+            )
+        else:
+            prompt_image, image, image_info = None, None, None
+            model_inputs = self.tokenizer(txt, max_length=self.tokenizer_max_length + drop_idx, padding=True, truncation=True, return_tensors="pt").to(self.device)
+            encoder_hidden_states = self.text_encoder(
+                input_ids=model_inputs.input_ids,
+                attention_mask=model_inputs.attention_mask,
+                output_hidden_states=True,
+            )
+
         hidden_states = encoder_hidden_states.hidden_states[-1]
-        split_hidden_states = self._extract_masked_hidden(hidden_states, txt_tokens.attention_mask)
+        split_hidden_states = self._extract_masked_hidden(hidden_states, model_inputs.attention_mask)
         split_hidden_states = [e[drop_idx:] for e in split_hidden_states]
         attn_mask_list = [torch.ones(e.size(0), dtype=torch.long, device=e.device) for e in split_hidden_states]
         max_seq_len = max([e.size(0) for e in split_hidden_states])
@@ -51,4 +137,4 @@ class Qwen25_VLForConditionalGeneration_TextEncoder:
         prompt_embeds = prompt_embeds.view(self.config.batchsize * self.config.num_images_per_prompt, seq_len, -1)
         prompt_embeds_mask = prompt_embeds_mask.repeat(1, self.config.num_images_per_prompt, 1)
         prompt_embeds_mask = prompt_embeds_mask.view(self.config.batchsize * self.config.num_images_per_prompt, seq_len)
-        return prompt_embeds, prompt_embeds_mask
+        return prompt_embeds, prompt_embeds_mask, image, image_info
---
+++ scripts/qwen_image/qwen_image_i2i.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=
+
+# set path and first
+export lightx2v_path=
+export model_path=
+
+# check section
+if [ -z "${CUDA_VISIBLE_DEVICES}" ]; then
+    cuda_devices=0
+    echo "Warn: CUDA_VISIBLE_DEVICES is not set, using default value: ${cuda_devices}, change at shell script or set env variable."
+    export CUDA_VISIBLE_DEVICES=${cuda_devices}
+fi
+
+if [ -z "${lightx2v_path}" ]; then
+    echo "Error: lightx2v_path is not set. Please set this variable first."
+    exit 1
+fi
+
+if [ -z "${model_path}" ]; then
+    echo "Error: model_path is not set. Please set this variable first."
+    exit 1
+fi
+
+export TOKENIZERS_PARALLELISM=false
+
+export PYTHONPATH=${lightx2v_path}:$PYTHONPATH
+export DTYPE=BF16
+export ENABLE_PROFILING_DEBUG=true
+export ENABLE_GRAPH_MODE=false
+
+
+python -m lightx2v.infer \
+    --model_cls qwen_image \
+    --task i2i \
+    --model_path $model_path \
+    --config_json ${lightx2v_path}/configs/qwen_image/qwen_image_i2i.json \
+    --prompt "Change the rabbit's color to purple, with a flash light background." \
+    --image_path input.jpg \
+    --save_video_path ${lightx2v_path}/save_results/qwen_image_i2i.png
