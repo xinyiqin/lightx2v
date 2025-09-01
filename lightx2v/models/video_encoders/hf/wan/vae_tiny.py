375a6f77e81b8fc21f4e767909e4fe41e81fbf45
gushiqiao <77222802+gushiqiao@users.noreply.github.com>
2025-09-01 19:19:16 +0800

    [Feat] support tae for wan_2_2 (#275)

--- lightx2v/models/video_encoders/hf/wan/vae_tiny.py
+++ lightx2v/models/video_encoders/hf/wan/vae_tiny.py
@@ -18,10 +18,8 @@ class WanVAE_tiny(nn.Module):
         self.device = torch.device("cuda")
         self.taehv = TAEHV(vae_pth).to(self.dtype)
         self.temperal_downsample = [True, True, False]
-        self.config = DotDict(scaling_factor=1.0, latents_mean=torch.zeros(16), z_dim=16, latents_std=torch.ones(16))
         self.need_scaled = need_scaled
 
-        # temp
         if self.need_scaled:
             self.latents_mean = [
                 -0.7571,
@@ -62,6 +60,132 @@ class WanVAE_tiny(nn.Module):
             ]
 
             self.z_dim = 16
+
+    @peak_memory_decorator
+    @torch.no_grad()
+    def decode(self, latents):
+        latents = latents.unsqueeze(0)
+
+        if self.need_scaled:
+            latents_mean = torch.tensor(self.latents_mean).view(1, self.z_dim, 1, 1, 1).to(latents.device, latents.dtype)
+            latents_std = 1.0 / torch.tensor(self.latents_std).view(1, self.z_dim, 1, 1, 1).to(latents.device, latents.dtype)
+            latents = latents / latents_std + latents_mean
+
+        # low-memory, set parallel=True for faster + higher memory
+        return self.taehv.decode_video(latents.transpose(1, 2).to(self.dtype), parallel=False).transpose(1, 2).mul_(2).sub_(1)
+
+
+class Wan2_2_VAE_tiny(nn.Module):
+    def __init__(self, vae_pth="taew2_2.pth", dtype=torch.bfloat16, device="cuda", need_scaled=False):
+        super().__init__()
+        self.dtype = dtype
+        self.device = torch.device("cuda")
+        self.taehv = TAEHV(vae_pth).to(self.dtype)
+        self.need_scaled = need_scaled
+        if self.need_scaled:
+            self.latents_mean = [
+                -0.2289,
+                -0.0052,
+                -0.1323,
+                -0.2339,
+                -0.2799,
+                0.0174,
+                0.1838,
+                0.1557,
+                -0.1382,
+                0.0542,
+                0.2813,
+                0.0891,
+                0.1570,
+                -0.0098,
+                0.0375,
+                -0.1825,
+                -0.2246,
+                -0.1207,
+                -0.0698,
+                0.5109,
+                0.2665,
+                -0.2108,
+                -0.2158,
+                0.2502,
+                -0.2055,
+                -0.0322,
+                0.1109,
+                0.1567,
+                -0.0729,
+                0.0899,
+                -0.2799,
+                -0.1230,
+                -0.0313,
+                -0.1649,
+                0.0117,
+                0.0723,
+                -0.2839,
+                -0.2083,
+                -0.0520,
+                0.3748,
+                0.0152,
+                0.1957,
+                0.1433,
+                -0.2944,
+                0.3573,
+                -0.0548,
+                -0.1681,
+                -0.0667,
+            ]
+
+            self.latents_std = [
+                0.4765,
+                1.0364,
+                0.4514,
+                1.1677,
+                0.5313,
+                0.4990,
+                0.4818,
+                0.5013,
+                0.8158,
+                1.0344,
+                0.5894,
+                1.0901,
+                0.6885,
+                0.6165,
+                0.8454,
+                0.4978,
+                0.5759,
+                0.3523,
+                0.7135,
+                0.6804,
+                0.5833,
+                1.4146,
+                0.8986,
+                0.5659,
+                0.7069,
+                0.5338,
+                0.4889,
+                0.4917,
+                0.4069,
+                0.4999,
+                0.6866,
+                0.4093,
+                0.5709,
+                0.6065,
+                0.6415,
+                0.4944,
+                0.5726,
+                1.2042,
+                0.5458,
+                1.6887,
+                0.3971,
+                1.0600,
+                0.3943,
+                0.5537,
+                0.5444,
+                0.4089,
+                0.7468,
+                0.7744,
+            ]
+
+            self.z_dim = 48
 
     @peak_memory_decorator
     @torch.no_grad()
