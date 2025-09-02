6de0a3b45114d7592eccc69ccf19bf2c2f9ce64e
Yang Yong(雍洋) <yongyang1030@163.com>
2025-09-02 22:41:19 +0800

    Add audio input files and update pre-commit config for larger files (#283)

--- scripts/seko_talk/run_seko_talk_11_fp8_dist_fixed_shape.sh
+++ scripts/seko_talk/run_seko_talk_11_fp8_dist_fixed_shape.sh
@@ -18,8 +18,8 @@ torchrun --nproc-per-node 4 -m lightx2v.infer \
 --task i2v \
 --model_path $model_path \
 --config_json ${lightx2v_path}/configs/seko_talk/seko_talk_11_fp8_dist_fixed_shape.json \
---prompt  "The video features a old lady is saying something and knitting a sweater." \
+--prompt  "The video features a male speaking to the camera with arms spread out, a slightly furrowed brow, and a focused gaze." \
 --negative_prompt 色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走 \
---image_path ${lightx2v_path}/assets/inputs/audio/15.png \
---audio_path ${lightx2v_path}/assets/inputs/audio/15.wav \
+--image_path ${lightx2v_path}/assets/inputs/audio/seko_input.png \
+--audio_path ${lightx2v_path}/assets/inputs/audio/seko_input.wav \
 --save_video_path ${lightx2v_path}/save_results/output_lightx2v_seko_talk.mp4
