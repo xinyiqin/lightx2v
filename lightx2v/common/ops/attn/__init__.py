8b1e4f944b4ddb54fb884846089d90220dd50fc9
Yang Yong (雍洋) <yongyang1030@163.com>
2025-10-21 11:28:14 +0800

    Support SVG2 (#384)

--- lightx2v/common/ops/attn/__init__.py
+++ lightx2v/common/ops/attn/__init__.py
@@ -2,6 +2,7 @@ from .flash_attn import FlashAttn2Weight, FlashAttn3Weight
 from .radial_attn import RadialAttnWeight
 from .ring_attn import RingAttnWeight
 from .sage_attn import SageAttn2Weight
+from .svg2_attn import Svg2AttnWeight
 from .svg_attn import SvgAttnWeight
 from .torch_sdpa import TorchSDPAWeight
 from .ulysses_attn import UlyssesAttnWeight
