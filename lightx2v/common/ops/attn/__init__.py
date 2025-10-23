9bb7dfe978a7a40a2116774c02940ee45668034c
Rongjin Yang <101640332+Linboyan-trc@users.noreply.github.com>
2025-10-23 18:10:30 +0800

    add spas_sage_attn (#402)
    https://github.com/Linboyan-trc/SpargeAttn/tree/02_develop
    
    need install
    
    ---------
    
    Co-authored-by: Yang Yong (雍洋) <yongyang1030@163.com>

--- lightx2v/common/ops/attn/__init__.py
+++ lightx2v/common/ops/attn/__init__.py
@@ -2,6 +2,7 @@ from .flash_attn import FlashAttn2Weight, FlashAttn3Weight
 from .radial_attn import RadialAttnWeight
 from .ring_attn import RingAttnWeight
 from .sage_attn import SageAttn2Weight
+from .spassage_attn import SageAttnWeight
 from .svg2_attn import Svg2AttnWeight
 from .svg_attn import SvgAttnWeight
 from .torch_sdpa import TorchSDPAWeight
