1f7bad54c3ca244769d3f5e03eb0eed0960ca4d0
fuheaven <43608858+fuheaven@users.noreply.github.com>
2025-12-09 17:41:27 +0800

    add dcu platform (#584)

--- lightx2v_platform/ops/__init__.py
+++ lightx2v_platform/ops/__init__.py
@@ -1,5 +1,12 @@
+import os
+
 from lightx2v_platform.base.global_var import AI_DEVICE
 
 if AI_DEVICE == "mlu":
     from .attn.cambricon_mlu import *
     from .mm.cambricon_mlu import *
+elif AI_DEVICE == "cuda":
+    # Check if running on DCU platform
+    if os.getenv("PLATFORM") == "dcu":
+        from .attn.dcu import *
+        from .mm.dcu import *
