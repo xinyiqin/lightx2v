1205be325b5e593f1b78b1eae518d00d3598e65f
yihuiwen <617954457@qq.com>
2025-12-26 14:08:26 +0800

    animate add padding (#654)
    Co-authored-by: yihuiwen <yihuiwen@sensetime.com>

--- lightx2v/models/networks/wan/model.py
+++ lightx2v/models/networks/wan/model.py
@@ -56,6 +56,7 @@ class WanModel(CompiledMethodsMixin):
         else:
             self.seq_p_group = None
 
+        self.padding_multiple = self.config.get("padding_multiple", 1)
         self.clean_cuda_cache = self.config.get("clean_cuda_cache", False)
         self.dit_quantized = self.config.get("dit_quantized", False)
         if self.dit_quantized:
@@ -482,7 +483,8 @@ class WanModel(CompiledMethodsMixin):
         world_size = dist.get_world_size(self.seq_p_group)
         cur_rank = dist.get_rank(self.seq_p_group)
 
-        padding_size = (world_size - (x.shape[0] % world_size)) % world_size
+        multiple = world_size * self.padding_multiple
+        padding_size = (multiple - (x.shape[0] % multiple)) % multiple
         if padding_size > 0:
             x = F.pad(x, (0, 0, 0, padding_size))
 
